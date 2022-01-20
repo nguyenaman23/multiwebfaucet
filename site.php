f36c1c798c566221367994f8124c7e3efa4e8d8c
iewilmaestro <83399918+iewilmaestro@users.noreply.github.com>
2022-01-20 15:44:13 +0700

    Add files via upload

---
+++ site.php
@@ -0,0 +1,30 @@
+<?php
+/*
+*
+*Creator	: iewil
+*Github		: https://github.com/iewilmaestro
+*Youtube	: https://www.youtube.com/c/iewil
+*Telegram	: @iewil57
+*Support	: Team-Function-INDO
+*
+*/
+require "cfg.php";
+$data = [[
+"host"=>"mcmfaucets.xyz",
+"cookie"=>$mcm
+],[
+"host"=>"dragonfaucets.xyz",
+"cookie"=>$dragon
+],[
+"host"=>"bestautofaucet.com",
+"cookie"=>$best
+],[
+"host"=>"auto.xtrabits.click",
+"cookie"=>$xtra
+],[
+"host"=>"phoenixfaucets.xyz",
+"cookie"=>$phoenix
+],[
+"host"=>"jupiterfaucet.net",
+"cookie"=>$jupi
+]];
